c2633ed57b36de1c0efa440def848ffe9503c386
riyazahuja <riyaza@andrew.cmu.edu>
2025-10-11 14:25:58 -0400

    added verifiedAgora dependency

--- lakefile.lean
+++ lakefile.lean
@@ -16,6 +16,8 @@ require "leanprover-community" / "batteries" @ git "v4.17.0"
 require "leanprover" / "Cli" @ git "v4.17.0"
 
 
+require VerifiedAgora from git
+  "https://github.com/stagiralabs/VerifiedAgora.git" @ "v4.17.0"
 
 @[default_target]
 lean_lib Library where
