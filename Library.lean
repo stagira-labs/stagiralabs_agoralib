002c739a58f6fe0dccb1bfe4b9c8262930f2da73
riyazahuja <riyaza@andrew.cmu.edu>
2025-10-12 16:03:52 +0000

    Update agoralib from LeanInterface

--- Library.lean
+++ Library.lean
@@ -1,2 +1,3 @@
 -- Import every contribution from Library
+import Library.targ_test
 import Library.test_foo
---
+++ Library/targ_test.lean
@@ -0,0 +1,5 @@
+-- Submitted by: test, Time: 88014251039/50, Name: targ_test
+import VerifiedAgora.tagger
+
+@[target]
+theorem da : 1 = 1 := by sorry
