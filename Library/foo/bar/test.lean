ff8b9ee1bc9cf8f159434f05c42538d6c09e0b2b
riyazahuja <riyaza@andrew.cmu.edu>
2025-10-12 18:35:37 +0000

    Update agoralib from LeanInterface

--- Library/foo/bar/test.lean
+++ Library/foo/bar/test.lean
@@ -1,4 +1,5 @@
+-- Submitted by: test, Time: 220036766651/125, Name: foo/bar/test
 -- Submitted by: test, Time: 1760294081451/1000, Name: foo/bar/test
 
 
-theorem nested_contrib_test : 1=1:=by rfl
+theorem nested_contrib_test : 1=1:= rfl
