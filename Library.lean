1a30f3b403ef194d6a6d909c7bfb211c9b371bf6
riyazahuja <riyaza@andrew.cmu.edu>
2025-10-12 18:21:39 +0000

    Update agoralib from LeanInterface

--- Library.lean
+++ Library.lean
@@ -5,6 +5,7 @@ import Library.duh
 import Library.file_kwf98hlx
 import Library.file_kwf98hlx_2
 import Library.file_ujm963i7
+import Library.foo.bar.test
 import Library.targ_mod
 import Library.targ_mod_2
 import Library.targ_test
---
+++ Library/foo/bar/test.lean
@@ -0,0 +1,2 @@
+-- Submitted by: test, Time: 880146647447/500, Name: foo/bar/test
+theorem subdir_test : 1=1 := by rfl
