1cd478d4e14dec0f2f843ea424861599070aab5f
riyazahuja <riyaza@andrew.cmu.edu>
2025-10-12 16:27:46 +0000

    Update agoralib from LeanInterface

--- Library.lean
+++ Library.lean
@@ -1,4 +1,5 @@
 -- Import every contribution from Library
+import Library.file_kwf98hlx
 import Library.file_ujm963i7
 import Library.targ_test
 import Library.test_foo
---
+++ Library/file_kwf98hlx.lean
@@ -0,0 +1,2 @@
+-- Submitted by: test, Time: 88014323183/50, Name: file_kwf98hlx
+theorem nested : 1 = 1 := by rfl
