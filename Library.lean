1f320150daaed14802f264629d107961eb838c86
riyazahuja <riyaza@andrew.cmu.edu>
2025-10-12 17:30:10 +0000

    Update agoralib from LeanInterface

--- Library.lean
+++ Library.lean
@@ -1,5 +1,6 @@
 -- Import every contribution from Library
 import Library.contrib_mod
+import Library.contrib_mod_2
 import Library.duh
 import Library.file_kwf98hlx
 import Library.file_kwf98hlx_2
---
+++ Library/contrib_mod_2.lean
@@ -0,0 +1,4 @@
+-- Submitted by: test, Time: 880145103197/500, Name: contrib_mod_2
+
+
+theorem contrib_mod_thm : 1= 1 := rfl
