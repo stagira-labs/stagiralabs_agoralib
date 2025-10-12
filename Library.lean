2ccd608264940585e6b8cdf98557a06ca3b7ac60
riyazahuja <riyaza@andrew.cmu.edu>
2025-10-12 16:14:24 +0000

    Update agoralib from LeanInterface

--- Library.lean
+++ Library.lean
@@ -1,3 +1,4 @@
 -- Import every contribution from Library
+import Library.file_ujm963i7
 import Library.targ_test
 import Library.test_foo
---
+++ Library/file_ujm963i7.lean
@@ -0,0 +1,2 @@
+-- Submitted by: test, Time: 1760285659787/1000, Name: file_ujm963i7
+theorem baz : 2=2 := by rfl
