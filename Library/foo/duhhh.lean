80dad9803f6a3c54c021fcf2efd15db3550c0588
riyazahuja <riyaza@andrew.cmu.edu>
2025-10-13 02:21:19 +0000

    Update agoralib from LeanInterface

--- Library/foo/duhhh.lean
+++ Library/foo/duhhh.lean
@@ -1,6 +1,6 @@
--- Submitted by: test, Time: 1760315092721/1000, Name: foo/duhhh
+-- Submitted by: test, Time: 1760322070801/1000, Name: foo/duhhh
 import VerifiedAgora.tagger
 
 
 @[target]
-theorem duhh : 1= 1 := by sorry
+theorem duhh : 1= 1 := by rfl
