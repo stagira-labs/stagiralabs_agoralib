64b92b146d61feffc6f7e6ff249ee1f3ff8d5056
AlexWilloughby3 <awilloug@andrew.cmu.edu>
2025-10-19 23:08:58 +0000

    Update agoralib from LeanInterface

--- Library.lean
+++ Library.lean
@@ -1,5 +1,6 @@
 -- Import every contribution from Library
 import Library.test
 import Library.test2
+import Library.test3
 import Library.text.spp
 import Library.text.x
---
+++ Library/test3.lean
@@ -0,0 +1,4 @@
+import VerifiedAgora.tagger
+
+@[target]
+theorem test_target_3 : 1 = 1 := by sorry
