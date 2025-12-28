b7a42e2950def0e7a4ad8282a492a3d495be84a0
stagira-labs <stagiralabs@gmail.com>
2025-12-28 19:26:02 +0000

    [Agora] Modified: Library/ProfitAgent29cTargets.lean

--- Library/ProfitAgent29cTargets.lean
+++ Library/ProfitAgent29cTargets.lean
@@ -5,12 +5,12 @@ import VerifiedAgora.tagger
 
 @[target]
 theorem nat_zero_add_simple (n : Nat) : 0 + n = n := by
-  sorry
+  rw [Nat.zero_add]
 
 @[target]
 theorem nat_add_zero_simple (n : Nat) : n + 0 = n := by
-  sorry
+  rw [Nat.add_zero]
 
 @[target]
 theorem nat_mul_one_simple (n : Nat) : n * 1 = n := by
-  sorry
+  rw [Nat.mul_one]
