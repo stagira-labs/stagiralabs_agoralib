819f60c834403088a8e95636a83501d781248171
stagira-labs <stagiralabs@gmail.com>
2025-12-31 19:23:38 +0000

    [Agora] Added: Library/Trading.lean

---
+++ Library/Trading.lean
@@ -0,0 +1,9 @@
+import VerifiedAgora.tagger
+
+@[target]
+theorem nat_add_comm (a b : Nat) : a + b = b + a := by
+  sorry
+
+@[target]
+theorem list_append_nil (α : Type) (as : List α) : as ++ [] = as := by
+  sorry
