9e0214fdfc8e8e40219178d1a4d33b9df0a9a575
stagira-labs <stagiralabs@gmail.com>
2025-12-31 21:26:54 +0000

    [Agora] Added: Library/betting_targets.lean

--- Library.lean
+++ Library.lean
@@ -2,6 +2,7 @@
 import Library.Agent3Targets
 import Library.MarketTargets
 import Library.advanced_targets
+import Library.betting_targets
 import Library.math_demo
 import Library.strategic_targets
 import Library.target_tagging_demo
