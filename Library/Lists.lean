ed95b0c116a94339bb3bf4424761a209ede60610
stagira-labs <stagiralabs@gmail.com>
2025-12-31 20:10:13 +0000

    [Agora] Modified: Library/Lists.lean

--- Library/Lists.lean
+++ Library/Lists.lean
@@ -4,12 +4,12 @@ import VerifiedAgora.tagger
 theorem list_append_nil (α : Type u) (l : List α) : l ++ [] = l := by
   induction l with
   | nil => rfl
-  | cons head tail ih =>
+  | cons head tail ih => 
     simp [ih]
 
 @[target]
 theorem list_append_assoc (α : Type u) (l1 l2 l3 : List α) : (l1 ++ l2) ++ l3 = l1 ++ (l2 ++ l3) := by
   induction l1 with
   | nil => rfl
-  | cons head tail ih =>
+  | cons head tail ih => 
     simp [ih]
