f8cbce99fe8901a7cc40fb8d3c780569136ed4f6
stagira-labs <stagiralabs@gmail.com>
2025-12-31 19:49:54 +0000

    [Agora] Modified: Library/TradingFloor.lean

--- Library/TradingFloor.lean
+++ Library/TradingFloor.lean
@@ -2,8 +2,8 @@ import VerifiedAgora.tagger
 
 @[target]
 theorem hidden_identity_77 (n : Nat) : n + 0 = n := by
-  sorry
+  rw [Nat.add_zero]
 
 @[target]
 theorem hidden_succ_77 (n : Nat) : Nat.succ n = n + 1 := by
-  sorry
+  rw [Nat.succ_eq_add_one]
