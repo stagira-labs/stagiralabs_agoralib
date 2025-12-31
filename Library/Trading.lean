95360d90b513178208f73e0b8092d6179b5b29ae
stagira-labs <stagiralabs@gmail.com>
2025-12-31 19:23:57 +0000

    [Agora] Modified: Library/Trading.lean

--- Library/Trading.lean
+++ Library/Trading.lean
@@ -2,8 +2,12 @@ import VerifiedAgora.tagger
 
 @[target]
 theorem nat_add_comm (a b : Nat) : a + b = b + a := by
-  sorry
+  induction a with
+  | zero => simp
+  | succ a ih => simp [ih, Nat.add_comm]
 
 @[target]
 theorem list_append_nil (α : Type) (as : List α) : as ++ [] = as := by
-  sorry
+  induction as with
+  | nil => rfl
+  | cons h t ih => simp [ih]
