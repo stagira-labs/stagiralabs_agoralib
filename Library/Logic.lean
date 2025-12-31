02dfcd18da64eb8e523b83e6631f484948b37b5e
stagira-labs <stagiralabs@gmail.com>
2025-12-31 20:13:36 +0000

    [Agora] Modified: Library/Logic.lean

--- Library/Logic.lean
+++ Library/Logic.lean
@@ -2,4 +2,15 @@ import VerifiedAgora.tagger
 
 @[target]
 theorem de_morgan_2 (p q : Prop) : ¬(p ∧ q) ↔ ¬p ∨ ¬q := by
-  sorry
+  constructor
+  · intro h
+    by_cases hp : p
+    · right
+      intro hq
+      exact h ⟨hp, hq⟩
+    · left
+      exact hp
+  · intro h ⟨hp, hq⟩
+    cases h with
+    | inl hnp => exact hnp hp
+    | inr hnq => exact hnq hq
