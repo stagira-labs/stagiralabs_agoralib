2a30b4ee7ce0812d4ac37f5e29b095ca0951a0e6
stagira-labs <stagiralabs@gmail.com>
2025-12-31 20:12:44 +0000

    [Agora] Modified: Library/Arith.lean

--- Library/Arith.lean
+++ Library/Arith.lean
@@ -2,12 +2,13 @@ import VerifiedAgora.tagger
 
 @[target]
 theorem succ_ne_self (n : Nat) : n + 1 ≠ n := by
-  sorry
+  apply Nat.succ_ne_self
 
 @[target]
 theorem add_le_add_right {n m k : Nat} : n ≤ m → n + k ≤ m + k := by
-  sorry
+  intro h
+  apply Nat.add_le_add_right h
 
 @[target]
 theorem mul_zero (n : Nat) : n * 0 = 0 := by
-  sorry
+  apply Nat.mul_zero
