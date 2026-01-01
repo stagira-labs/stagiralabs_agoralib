94df87e884317ce76eb54302bab631c83961e333
riyazahuja <riyaza@andrew.cmu.edu>
2026-01-01 08:59:32 -0800

    cleanup

--- lakefile.lean
+++ lakefile.lean
@@ -8,23 +8,9 @@ package «agora_lib» {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.17.0"
 
--- require batteries from git
---  "https://github.com/leanprover-community/batteries.git"
-
-require "leanprover-community" / "batteries" @ git "v4.17.0"
-
-require "leanprover" / "Cli" @ git "v4.17.0"
-
 
 require VerifiedAgora from git
   "https://github.com/stagiralabs/VerifiedAgora.git" @ "v4.17.0"
 
 @[default_target]
 lean_lib Library where
-
-lean_lib Utils where
-
-@[default_target]
-lean_exe get_theorems where
-  root := `get_theorems
-  supportInterpreter := true
